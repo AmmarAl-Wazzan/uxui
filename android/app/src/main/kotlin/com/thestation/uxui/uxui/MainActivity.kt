457463c9122e7bf84d27b7e4ad68dec35dc162a0
Ammar AlWazzan <ammar.alwazan@gmail.com>
2022-02-22 17:38:23 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/thestation/uxui/uxui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thestation.uxui.uxui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.red,
+                  height: 120,
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Container(
+                  color: Colors.amber,
+                  height: 120,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
